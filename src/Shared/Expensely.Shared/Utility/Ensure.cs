eaf0b4709a0dfec48045965a17b98286f95fe707
Milan Jovanović <milannis.995@gmail.com>
2020-06-27 18:31:36 +0200

    - Added shared project that will cover cross-cutting concerns and utility classes

---
+++ src/Shared/Expensely.Shared/Utility/Ensure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Expensely.Shared.Utility
+{
+    /// <summary>
+    /// Contains assertions for the most common application checks.
+    /// </summary>
+    public static class Ensure
+    {
+        /// <summary>
+        /// Ensures that the specified <see cref="Guid"/> value is not empty.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <param name="message">The message to show if the check fails.</param>
+        /// <param name="argumentName">The name of the argument being checked.</param>
+        /// <exception cref="ArgumentException"> if the specified value is empty.</exception>
+        public static void NotEmpty(Guid value, string message, string argumentName)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new ArgumentException(message, argumentName);
+            }
+        }
+
+        /// <summary>
+        /// Checks that the specified <see cref="string"/> value is not empty.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <param name="message">The message to show if the check fails.</param>
+        /// <param name="argumentName">The name of the argument being checked.</param>
+        /// <exception cref="ArgumentException"> if the specified value is empty.</exception>
+        public static void NotEmpty(string value, string message, string argumentName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(message, argumentName);
+            }
+        }
+    }
+}
