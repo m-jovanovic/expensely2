ebe84659292c897260cfde43f21dfb24f847ec65
Milan Jovanović <milannis.995@gmail.com>
2020-06-28 14:01:32 +0200

    - Added missing interfaces that caused build to crash

---
+++ src/Domain/Expensely.Domain/ISoftDeletableEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Expensely.Domain
+{
+    /// <summary>
+    /// Represents the marker interface for soft-deletable entities.
+    /// </summary>
+    public interface ISoftDeletableEntity
+    {
+        /// <summary>
+        /// Gets a value indicating whether the entity has been deleted.
+        /// </summary>
+        bool Deleted { get; }
+    }
+}
