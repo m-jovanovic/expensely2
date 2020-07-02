7eed579fb8c0ea04df324d76b553355160b74c5f
Milan Jovanović <milannis.995@gmail.com>
2020-07-02 15:03:12 +0200

    - Added Money value object and unit tests for it

---
+++ src/Domain/Expensely.Domain/ValueObjects/Money.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Expensely.Domain.Primitives;
+
+namespace Expensely.Domain.ValueObjects
+{
+    public class Money : ValueObject
+    {
+        public Money(decimal amount, Currency currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public decimal Amount { get; }
+
+        public Currency Currency { get; }
+
+        public static Money operator +(Money left, Money right)
+        {
+            if (left.Currency != right.Currency)
+            {
+                throw new InvalidOperationException($"Can not add currencies {left.Currency.Code} and {right.Currency.Code}.");
+            }
+
+            return new Money(left.Amount + right.Amount, left.Currency);
+        }
+
+        public static Money operator -(Money left, Money right)
+        {
+            if (left.Currency != right.Currency)
+            {
+                throw new InvalidOperationException($"Can not add currencies {left.Currency.Code} and {right.Currency.Code}.");
+            }
+
+            return new Money(left.Amount - right.Amount, left.Currency);
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Amount;
+            yield return Currency;
+        }
+    }
+}
---
+++ tests/Expensely.Domain.Tests/ValueObjects/MoneyTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Expensely.Domain.ValueObjects;
+using Xunit;
+
+namespace Expensely.Domain.Tests.ValueObjects
+{
+    public class MoneyTests
+    {
+        private static readonly Currency _currency = Currency.AllCurrencies[0];
+
+        [Fact]
+        public void Should_ConstructProperly()
+        {
+            var money = new Money(0, _currency);
+
+            Assert.Equal(0, money.Amount);
+            Assert.Equal(Currency.Usd, money.Currency);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(-1, 1)]
+        [InlineData(12.01049102412, 15151.12312312954190)]
+        [InlineData(-13123, -123123123)]
+        public void Should_AddProperly(decimal amount1, decimal amount2)
+        {
+            var money1 = new Money(amount1, _currency);
+            var money2 = new Money(amount2, _currency);
+
+            Money addedMoney = money1 + money2;
+
+            Assert.Equal(addedMoney.Amount, amount1 + amount2);
+            Assert.Equal(addedMoney.Currency, _currency);
+        }
+
+        [Fact]
+        public void Should_ThrowInvalidOperationException_WhenAddingDifferentCurrencies()
+        {
+            var money1 = new Money(0, Currency.Usd);
+            var money2 = new Money(0, Currency.Eur);
+
+            Assert.Throws<InvalidOperationException>(() => money1 + money2);
+        }
+
+        [Fact]
+        public void Should_CreateNewInstance_WhenAdding()
+        {
+            var money1 = new Money(0, _currency);
+            var money2 = new Money(0, _currency);
+
+            Money addedMoney = money1 + money2;
+
+            Assert.NotSame(addedMoney, money1);
+            Assert.NotSame(addedMoney, money2);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(-1, 1)]
+        [InlineData(12.01049102412, 15151.12312312954190)]
+        [InlineData(-13123, -123123123)]
+        public void Should_SubtractProperly(decimal amount1, decimal amount2)
+        {
+            var money1 = new Money(amount1, _currency);
+            var money2 = new Money(amount2, _currency);
+
+            Money addedMoney = money1 - money2;
+
+            Assert.Equal(addedMoney.Amount, amount1 - amount2);
+            Assert.Equal(addedMoney.Currency, _currency);
+        }
+
+        [Fact]
+        public void Should_ThrowInvalidOperationException_WhenSubtractingDifferentCurrencies()
+        {
+            var money1 = new Money(0, Currency.Usd);
+            var money2 = new Money(0, Currency.Eur);
+
+            Assert.Throws<InvalidOperationException>(() => money1 - money2);
+        }
+
+        [Fact]
+        public void Should_CreateNewInstance_WhenSubtracting()
+        {
+            var money1 = new Money(0, _currency);
+            var money2 = new Money(0, _currency);
+
+            Money addedMoney = money1 - money2;
+
+            Assert.NotSame(addedMoney, money1);
+            Assert.NotSame(addedMoney, money2);
+        }
+    }
+}
