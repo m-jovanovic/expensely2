6048ba57e908ee6d156d4d52c864ecdf7883e4ab
Milan Jovanović <milannis.995@gmail.com>
2020-07-02 14:45:30 +0200

    - Added Currency value object and unit tests for it

---
+++ src/Domain/Expensely.Domain/ValueObjects/Currency.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Expensely.Domain.Primitives;
+
+namespace Expensely.Domain.ValueObjects
+{
+    public class Currency : ValueObject
+    {
+        public static readonly Currency Usd = new Currency(1, "USD", "$");
+        public static readonly Currency Eur = new Currency(2, "EUR", "€");
+        public static readonly Currency Rsd = new Currency(3, "RSD", "din.");
+
+        public static readonly IReadOnlyList<Currency> AllCurrencies = new List<Currency>
+        {
+            Usd,
+            Eur,
+            Rsd
+        };
+
+        private Currency(int id, string code, string sign)
+        {
+            Id = id;
+            Sign = sign;
+            Code = code;
+        }
+
+        public int Id { get; }
+
+        public string Code { get; }
+
+        public string Sign { get; }
+
+        public static Currency? FromId(int currencyId)
+            => ValueBetweenTwoValues(currencyId, 1, AllCurrencies.Count) ? AllCurrencies[currencyId - 1] : null;
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Id;
+            yield return Code;
+            yield return Sign;
+        }
+
+        private static bool ValueBetweenTwoValues(int value, int fromInclusive, int toInclusive)
+            => value >= fromInclusive && value <= toInclusive;
+    }
+}
---
+++ tests/Expensely.Domain.Tests/ValueObjects/CurrencyTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using Expensely.Domain.ValueObjects;
+using Xunit;
+
+namespace Expensely.Domain.Tests.ValueObjects
+{
+    public class CurrencyTests
+    {
+        public static IEnumerable<object[]> GetIdenticalCurrencies()
+        {
+            yield return new object[] { Currency.Usd, Currency.Usd };
+            yield return new object[] { Currency.Eur, Currency.Eur };
+            yield return new object[] { Currency.Rsd, Currency.Rsd };
+        }
+
+        public static IEnumerable<object[]> GetDifferentCurrencies()
+        {
+            yield return new object[] { Currency.Usd, Currency.Eur };
+            yield return new object[] { Currency.Usd, Currency.Rsd };
+            yield return new object[] { Currency.Eur, Currency.Usd };
+            yield return new object[] { Currency.Eur, Currency.Rsd };
+            yield return new object[] { Currency.Rsd, Currency.Usd };
+            yield return new object[] { Currency.Rsd, Currency.Eur };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetIdenticalCurrencies))]
+        public void Should_BeEqual_GivenSameCurrency(Currency currency1, Currency currency2)
+        {
+            Assert.Equal(currency1, currency2);
+            Assert.True(currency1 == currency2);
+            Assert.True(currency1.Equals(currency2));
+            Assert.True(currency1.GetHashCode() == currency2.GetHashCode());
+        }
+
+        [Theory]
+        [MemberData(nameof(GetDifferentCurrencies))]
+        public void Should_NotBeEqual_GivenSameCurrency(Currency currency1, Currency currency2)
+        {
+            Assert.NotEqual(currency1, currency2);
+            Assert.False(currency1 == currency2);
+            Assert.False(currency1.Equals(currency2));
+            Assert.False(currency1.GetHashCode() == currency2.GetHashCode());
+        }
+
+        [Fact]
+        public void FromId_Should_ReturnNull_GivenNegativeCurrencyId()
+        {
+            int currencyId = int.MinValue;
+
+            var currency = Currency.FromId(currencyId);
+
+            Assert.Null(currency);
+        }
+
+        [Fact]
+        public void FromId_Should_ReturnNull_GivenOutOfBoundsCurrencyId()
+        {
+            int currencyId = Currency.AllCurrencies.Count + 1;
+
+            var currency = Currency.FromId(currencyId);
+
+            Assert.Null(currency);
+        }
+
+        [Fact]
+        public void FromId_ShouldReturnCurrency_GivenLowestCurrencyId()
+        {
+            int currencyId = 1;
+
+            var currency = Currency.FromId(currencyId);
+
+            Assert.NotNull(currency);
+        }
+
+        [Fact]
+        public void FromId_ShouldReturnCurrency_GivenHighestCurrencyId()
+        {
+            int currencyId = Currency.AllCurrencies.Count;
+
+            var currency = Currency.FromId(currencyId);
+
+            Assert.NotNull(currency);
+        }
+    }
+}
