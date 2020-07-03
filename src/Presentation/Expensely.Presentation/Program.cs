d59b4adf8b85c8e8d748d2a365691efef0470d56
Milan Jovanović <milannis.995@gmail.com>
2020-07-03 11:50:27 +0200

    - Implemented listing expenses in Presentation app, added Material Design + Bootstrap UI framework

--- src/Presentation/Expensely.Presentation/Program.cs
+++ src/Presentation/Expensely.Presentation/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Expensely.Presentation.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,7 +15,12 @@ namespace Expensely.Presentation
 
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:5001")
+            });
+
+            builder.Services.AddScoped<ExpenseService>();
 
             await builder.Build().RunAsync();
         }
