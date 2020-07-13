c8080974e3da954b5487f86876954ddcb5c3c0e1
Milan Jovanović <milannis.995@gmail.com>
2020-07-13 19:27:30 +0200

    - Added DomainException class

---
+++ src/Domain/Expensely.Domain.Core/Exceptions/DomainException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Expensely.Domain.Core.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string errorCode)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public string ErrorCode { get; }
+    }
+}
