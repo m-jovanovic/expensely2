5b899825dbcc0ef442275e42ffadfff9693546b6
Milan Jovanović <milannis.995@gmail.com>
2020-08-15 00:29:10 +0200

    - Removed unnecessary data access tests

--- server/tests/Expensely.Application.UnitTests/Expenses/Queries/GetExpenseByIdQueryTests.cs
+++ server/tests/Expensely.Application.UnitTests/Expenses/Queries/GetExpenseByIdQueryTests.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Expensely.Application.Abstractions.Data;
 using Expensely.Application.Constants;
 using Expensely.Application.Contracts.Expenses;
 using Expensely.Application.Expenses.Queries.GetExpenseById;
-using Expensely.Domain.Entities;
-using Expensely.Tests.Common.Entities;
 using FluentAssertions;
-using MockQueryable.Moq;
 using Moq;
 using Xunit;
 
@@ -47,67 +42,6 @@ namespace Expensely.Application.UnitTests.Expenses.Queries
             ExpenseResponse? result = await queryHandler.Handle(query, default);
 
             result.Should().BeNull();
-        }
-
-        [Fact]
-        public async Task Should_return_null_if_expense_with_specified_id_does_not_exist()
-        {
-            var dbSetMock = new List<Expense>().AsQueryable().BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpenseByIdQueryHandler(dbContextMock.Object);
-            var query = new GetExpenseByIdQuery(Guid.NewGuid(), Guid.NewGuid());
-
-            ExpenseResponse? result = await queryHandler.Handle(query, default);
-
-            result.Should().BeNull();
-        }
-
-        [Fact]
-        public async Task Should_return_null_if_expense_with_specified_id_exists_but_user_id_is_invalid()
-        {
-            var expense = ExpenseData.CreateExpense();
-            var dbSetMock = new List<Expense>
-                {
-                    expense
-                }
-                .AsQueryable()
-                .BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpenseByIdQueryHandler(dbContextMock.Object);
-            var query = new GetExpenseByIdQuery(expense.Id, Guid.NewGuid());
-
-            ExpenseResponse result = (await queryHandler.Handle(query, default))!;
-
-            result.Should().BeNull();
-        }
-
-        [Fact]
-        public async Task Should_return_expense_response_if_expense_with_specified_id_and_user_id_exists()
-        {
-            var expense = ExpenseData.CreateExpense();
-            var dbSetMock = new List<Expense>
-                {
-                    expense
-                }
-                .AsQueryable()
-                .BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpenseByIdQueryHandler(dbContextMock.Object);
-            var query = new GetExpenseByIdQuery(expense.Id, expense.UserId);
-
-            ExpenseResponse result = (await queryHandler.Handle(query, default))!;
-
-            result.Should().NotBeNull();
-            result.Id.Should().Be(expense.Id);
-            result.Name.Should().Be(expense.Name);
-            result.Amount.Should().Be(expense.Money.Amount);
-            result.CurrencyId.Should().Be(expense.Money.Currency.Id);
-            result.CurrencyCode.Should().Be(expense.Money.Currency.Code);
-            result.Date.Should().Be(expense.Date);
-            result.CreatedOnUtc.Should().Be(expense.CreatedOnUtc);
         }
     }
 }
--- server/tests/Expensely.Application.UnitTests/Expenses/Queries/GetExpensesQueryTests.cs
+++ server/tests/Expensely.Application.UnitTests/Expenses/Queries/GetExpensesQueryTests.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Expensely.Application.Abstractions.Data;
 using Expensely.Application.Constants;
 using Expensely.Application.Contracts.Expenses;
 using Expensely.Application.Expenses.Queries.GetExpenses;
 using Expensely.Application.Utilities;
-using Expensely.Domain.Entities;
-using Expensely.Tests.Common;
-using Expensely.Tests.Common.Entities;
 using FluentAssertions;
-using MockQueryable.Moq;
 using Moq;
 using Xunit;
+using ISqlConnectionFactory = Expensely.Application.Abstractions.Data.ISqlConnectionFactory;
 
 namespace Expensely.Application.UnitTests.Expenses.Queries
 {
@@ -21,13 +15,6 @@ namespace Expensely.Application.UnitTests.Expenses.Queries
     {
         private const int Limit = 2;
         private static readonly Guid UserId = Guid.NewGuid();
-        private static readonly DateTime Date = Time.Now().Date;
-        private static readonly List<Expense> Expenses = new List<Expense>
-        {
-            ExpenseData.CreateExpense(UserId, Date),
-            ExpenseData.CreateExpense(UserId, Date.AddDays(-1)),
-            ExpenseData.CreateExpense(UserId, Date.AddDays(-2))
-        };
 
         [Fact]
         public void Should_construct_properly_with_cursor()
@@ -72,10 +59,7 @@ namespace Expensely.Application.UnitTests.Expenses.Queries
         [Fact]
         public async Task Should_return_empty_response_if_user_id_is_empty()
         {
-            var dbSetMock = Expenses.AsQueryable().BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpensesQueryHandler(dbContextMock.Object);
+            var queryHandler = new GetExpensesQueryHandler(new Mock<ISqlConnectionFactory>().Object);
             var query = new GetExpensesQuery(Guid.Empty, Limit, null, DateTime.UtcNow);
 
             ExpenseListResponse result = await queryHandler.Handle(query, default);
@@ -83,87 +67,6 @@ namespace Expensely.Application.UnitTests.Expenses.Queries
             result.Should().NotBeNull();
             result.Items.Should().NotBeNull();
             result.Items.Should().BeEmpty();
-        }
-
-        [Fact]
-        public async Task Should_return_empty_response_if_no_expenses_exist_for_user_id()
-        {
-            var dbSetMock = new List<Expense>().AsQueryable().BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpensesQueryHandler(dbContextMock.Object);
-            var query = new GetExpensesQuery(Guid.NewGuid(), Limit, null, DateTime.UtcNow);
-
-            ExpenseListResponse result = await queryHandler.Handle(query, default);
-
-            result.Should().NotBeNull();
-            result.Items.Should().NotBeNull();
-            result.Items.Should().BeEmpty();
-            result.Cursor.Should().BeEmpty();
-        }
-
-        [Fact]
-        public async Task Should_return_non_empty_response_if_expenses_exist_for_user_id()
-        {
-            var dbSetMock = Expenses.AsQueryable().BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpensesQueryHandler(dbContextMock.Object);
-            var query = new GetExpensesQuery(UserId, Limit, null, DateTime.UtcNow);
-
-            ExpenseListResponse result = await queryHandler.Handle(query, default);
-
-            result.Should().NotBeNull();
-            result.Items.Should().HaveCount(Limit);
-        }
-
-        [Fact]
-        public async Task Should_return_empty_cursor_if_no_more_expenses_exist_for_user_id()
-        {
-            var dbSetMock = Expenses.AsQueryable().BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpensesQueryHandler(dbContextMock.Object);
-            var query = new GetExpensesQuery(UserId, Expenses.Count, null, DateTime.UtcNow);
-
-            ExpenseListResponse result = await queryHandler.Handle(query, default);
-
-            result.Should().NotBeNull();
-            result.Items.Should().HaveCount(Expenses.Count);
-            result.Cursor.Should().BeEmpty();
-        }
-
-        [Fact]
-        public async Task Should_return_non_empty_cursor_if_more_expenses_exist_for_user_id()
-        {
-            var dbSetMock = Expenses.AsQueryable().BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpensesQueryHandler(dbContextMock.Object);
-            var query = new GetExpensesQuery(UserId, Limit, null, DateTime.UtcNow);
-
-            ExpenseListResponse result = await queryHandler.Handle(query, default);
-
-            result.Should().NotBeNull();
-            result.Cursor.Should().NotBeEmpty();
-        }
-
-        [Fact]
-        public async Task Should_return_more_expenses_with_cursor_from_response()
-        {
-            var dbSetMock = Expenses.AsQueryable().BuildMockDbSet();
-            var dbContextMock = new Mock<IDbContext>();
-            dbContextMock.Setup(x => x.Set<Expense>()).Returns(dbSetMock.Object);
-            var queryHandler = new GetExpensesQueryHandler(dbContextMock.Object);
-            var query = new GetExpensesQuery(UserId, Limit, null, DateTime.UtcNow);
-
-            ExpenseListResponse resultWithCursor = await queryHandler.Handle(query, default);
-            var queryWithCursor = new GetExpensesQuery(UserId, Limit, resultWithCursor.Cursor, DateTime.UtcNow);
-            ExpenseListResponse result = await queryHandler.Handle(queryWithCursor, default);
-
-            result.Should().NotBeNull();
-            result.Items.Should().NotBeEmpty();
-            result.Cursor.Should().BeEmpty();
         }
     }
 }
