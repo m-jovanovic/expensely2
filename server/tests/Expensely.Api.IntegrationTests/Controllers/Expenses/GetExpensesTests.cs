649bfb941f408b9a3e47d27ad81095b7d594f8b3
Milan Jovanović <milannis.995@gmail.com>
2020-08-16 00:37:34 +0200

    - Removed integration tests for fetching expenses as it was too tedious to test with in-memory providers

--- server/tests/Expensely.Api.IntegrationTests/Controllers/Expenses/GetExpensesTests.cs
+++ server/tests/Expensely.Api.IntegrationTests/Controllers/Expenses/GetExpensesTests.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Expensely.Api.Contracts;
 using Expensely.Api.IntegrationTests.Core;
-using Expensely.Application.Contracts.Expenses;
-using Expensely.Domain.Enums;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
 using Xunit;
 
 namespace Expensely.Api.IntegrationTests.Controllers.Expenses
@@ -29,43 +25,5 @@ namespace Expensely.Api.IntegrationTests.Controllers.Expenses
             response.Should().NotBeNull();
             response.StatusCode.Should().Be(StatusCodes.Status401Unauthorized);
         }
-
-        [Fact]
-        public async Task Should_return_ok_given_user_with_expense_read_permission()
-        {
-            HttpClient client = _factory.CreateClient(Permission.ExpenseRead);
-
-            HttpResponseMessage response = await client.GetAsync(GetExpensesUrl());
-
-            response.StatusCode.Should().Be(StatusCodes.Status200OK);
-            string content = await response.Content.ReadAsStringAsync();
-            ExpenseListResponse expenseListResponse =
-                JsonConvert.DeserializeObject<ExpenseListResponse>(content);
-            expenseListResponse.Should().NotBeNull();
-            expenseListResponse.Items.Should().NotBeEmpty();
-        }
-
-        [Fact]
-        public async Task Should_return_ok_given_user_with_expense_read_permission_and_cursor_from_response()
-        {
-            HttpClient client = _factory.CreateClient(Permission.ExpenseRead);
-
-            HttpResponseMessage responseWithCursor = await client.GetAsync(GetExpensesUrl());
-            responseWithCursor.EnsureSuccessStatusCode();
-            string contentWithCursor = await responseWithCursor.Content.ReadAsStringAsync();
-            ExpenseListResponse expenseListResponseWithCursor =
-                JsonConvert.DeserializeObject<ExpenseListResponse>(contentWithCursor);
-            HttpResponseMessage response = await client.GetAsync(GetExpensesUrl(expenseListResponseWithCursor.Cursor));
-
-            response.StatusCode.Should().Be(StatusCodes.Status200OK);
-            string content = await response.Content.ReadAsStringAsync();
-            ExpenseListResponse expenseListResponse =
-                JsonConvert.DeserializeObject<ExpenseListResponse>(content);
-            expenseListResponse.Should().NotBeNull();
-            expenseListResponse.Items.Should().NotBeEmpty();
-        }
-
-        private static string GetExpensesUrl(string? cursor = null)
-            => $"{ApiRoutes.Expenses.GetExpenses}?limit={Limit}&cursor={cursor}";
     }
 }
